22f52299bd270d2e12015f04a4d7635208bedd96
kevincastro015 <kevin.castro015@mymdc.net>
2021-07-05 18:05:01 +0000

    lightstructure

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,16 +1,15 @@
 import React from "react";
-import "relative/path/to/stylesheet.scss";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
 	return (
 		<div className="traffic-light">
-			<div className="light red">Red</div>
-			<div className="light yellow">Yellow</div>
-			<div className="light green">Green</div>
+			<div className="light red"></div>
+			<div className="light yellow"></div>
+			<div className="light green"></div>
 		</div>
 	);
 }
