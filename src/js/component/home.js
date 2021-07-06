78cf3276b4af5595a78a6efb23a2721270ed7469
kevincastro015 <kevin.castro015@mymdc.net>
2021-07-06 20:54:33 +0000

    onclickworking

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -8,22 +8,27 @@ export function Home() {
 	const [selectedColor, setSelectedColor] = useState("red");
 
 	return (
-		<div className="traffic-light">
-			<div
-				onClick={() => setSelectedColor === "red"}
-				className={
-					"light red" + (selectedColor === "red" ? " glow" : "")
-				}></div>
-			<div
-				onClick={() => setSelectedColor === "yellow"}
-				className={
-					"light yellow" + (selectedColor === "yellow" ? " glow" : "")
-				}></div>
-			<div
-				onClick={() => setSelectedColor === "green"}
-				className={
-					"light green" + (selectedColor === "green" ? " glow" : "")
-				}></div>
+		<div className="container">
+			<div className="traffic-top"></div>
+			<div className="traffic-light">
+				<div
+					onClick={() => setSelectedColor("red")}
+					className={
+						"light red" + (selectedColor === "red" ? " glow" : "")
+					}></div>
+				<div
+					onClick={() => setSelectedColor("yellow")}
+					className={
+						"light yellow" +
+						(selectedColor === "yellow" ? " glow" : "")
+					}></div>
+				<div
+					onClick={() => setSelectedColor("green")}
+					className={
+						"light green" +
+						(selectedColor === "green" ? " glow" : "")
+					}></div>
+			</div>
 		</div>
 	);
 }
